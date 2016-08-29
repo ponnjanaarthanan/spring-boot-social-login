977f913e04aea3b058db895d7d0b7acac32612e7
Ponn Janaarthanan Sundhararajan <Ponn Janaarthanan Sundhararajan>
2016-08-29 20:47:13 +0530

    Initial Spring Boot Setup

---
+++ src/main/java/com/jana/social/BootLoader.java
@@ -0,0 +1,12 @@
+package com.jana.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLoader {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootLoader.class, args);
+	}
+}
