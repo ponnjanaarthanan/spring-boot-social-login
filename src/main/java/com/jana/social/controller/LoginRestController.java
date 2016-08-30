d5e83dc925f92d89c192c6659b396c7ba9fc28c4
Ponn Janaarthanan Sundhararajan <Ponn Janaarthanan Sundhararajan>
2016-08-30 20:17:12 +0530

    Facebook JS SDK added

--- src/main/java/com/jana/social/controller/LoginRestController.java
+++ src/main/java/com/jana/social/controller/LoginRestController.java
@@ -1,19 +1,19 @@
 package com.jana.social.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginRestController {
 	
 	@RequestMapping("/")
 	public String index(){
-		return "Welcome to Spring Boot";
+		return "index";
 	}
 	
 	@RequestMapping("/login")
 	public String login(){
-		return "Login Redirection Comes here";
+		return "login";
 	}
 
 }
